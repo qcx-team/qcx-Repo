942126e49eaf1f28958df6a2466d702242f7c82a
WellChris <270618690@qq.com>
2020-01-22 10:24:28 +0800

    MyWebConfigurer 增加了拦截器

---
+++ src/main/java/com/xhj/qcxjob/bean/UserInfo.java
@@ -0,0 +1,75 @@
+package com.xhj.qcxjob.bean;
+
+import java.util.Date;
+
+public class UserInfo {
+    private Integer id;
+
+    private String userName;
+
+    private String passWord;
+
+    private String phone;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord == null ? null : passWord.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/com/xhj/qcxjob/mapper/UserInfoMapper.java
@@ -0,0 +1,35 @@
+package com.xhj.qcxjob.mapper;
+
+import com.xhj.qcxjob.bean.UserInfo;
+import com.xhj.qcxjob.bean.UserInfoExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UserInfoMapper {
+    long countByExample(UserInfoExample example);
+
+    int deleteByExample(UserInfoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    List<UserInfo> selectByExample(UserInfoExample example);
+
+    UserInfo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") UserInfo record, @Param("example") UserInfoExample example);
+
+    int updateByExample(@Param("record") UserInfo record, @Param("example") UserInfoExample example);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
